9069136b1159ef58ae972664655c03d9e5892ba6
SparkyProto <hannah@thespains.net>
2023-12-29 15:48:17 -0600

    prop detection

--- TeamCodeBeta/src/main/java/org/firstinspires/ftc/teamcodebeta/drive/config/BetaDriveConstants.java
+++ TeamCodeBeta/src/main/java/org/firstinspires/ftc/teamcodebeta/drive/config/BetaDriveConstants.java
@@ -91,6 +91,10 @@ public class BetaDriveConstants {
     public static double MAX_ANG_VEL = Math.toRadians(184.02607784577722);
     public static double MAX_ANG_ACCEL = Math.toRadians(184.02607784577722);
 
+    public static RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR =
+            RevHubOrientationOnRobot.LogoFacingDirection.UP;
+    public static RevHubOrientationOnRobot.UsbFacingDirection USB_FACING_DIR =
+            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
     public static double encoderTicksToInches(double ticks) {
         return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
