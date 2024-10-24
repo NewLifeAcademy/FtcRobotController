c937c71b77e4c3b8dbd5beef0ea6ed85cbf2608d
New Life Academy Robotics <150531206+nlarobotics@users.noreply.github.com>
2024-10-24 16:48:15 -0500

    Create Alpha Plan Blue Right

---
+++ TeamCodeAlpha/src/main/java/org/firstinspires/ftc/teamcodealpha/autonomous/AlphaPlanBlueRight.java
@@ -0,0 +1,113 @@
+/* Copyright (c) 2019 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcodealpha.autonomous;
+
+import android.util.Size;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcodealpha.AlphaBot2024;
+import org.firstinspires.ftc.teamcodealpha.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+/*
+ * This OpMode illustrates the basics of TensorFlow Object Detection,
+ * including Java Builder structures for specifying Vision parameterConceptTensorFlowObjectDetections.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
+ */
+@Autonomous(name = "Auto - Plan Blue Right", preselectTeleOp = "2023-2024 IronEagle-Strafe")
+
+@Config
+public class AlphaPlanBlueRight extends LinearOpMode {
+    public static double START_POS_X = -36.25;
+    public static double START_POS_Y = 62.5;
+    public static double START_POS_HEADING = 270;
+    public static double WAYP_ONE_X = -10;
+    public static double WAYP_ONE_Y = 27;
+    public static double WAYP_ONE_HEADING = 270;
+    public static double WAYP_TWO_X = -56.25;
+    public static double WAYP_TWO_Y = 62.5;
+    public static double WAYP_TWO_HEADING = 270;
+    public static double PARK_X = -60;
+    public static double PARK_Y = 60;
+    public static double PARK_HEADING = 270;
+
+
+
+
+    public void runOpMode() {
+        AlphaBot2024 drive = new AlphaBot2024(hardwareMap);
+
+
+
+        if (opModeIsActive()) {
+
+            while (opModeIsActive()) {
+                // Define the start pose
+                Pose2d startPose = new Pose2d(START_POS_X, START_POS_Y, Math.toRadians(START_POS_HEADING));
+                drive.setPoseEstimate(startPose);
+
+                //create waypoint 1
+                Pose2d waypOne = new Pose2d(WAYP_ONE_X, WAYP_ONE_Y, Math.toRadians(WAYP_ONE_HEADING));
+
+                //create trajectory to waypoint one and follow it
+                TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
+                        .lineToLinearHeading(waypOne)
+                        .build();
+
+                drive.followTrajectorySequence(trajSeq);
+
+                // TODO: place specimen
+
+                //create waypoint 2
+                Pose2d waypTwo = new Pose2d(WAYP_TWO_X, WAYP_TWO_Y, Math.toRadians(WAYP_TWO_HEADING));
+
+                //create trajectory to waypoint two and follow it
+                trajSeq = drive.trajectorySequenceBuilder(waypOne)
+                        .lineToLinearHeading(waypTwo)
+                        .build();
+
+
+                drive.followTrajectorySequence(trajSeq);
+            }
+
+
+        }
+    }   // end runOpMode()
+
+
+}   // end class
