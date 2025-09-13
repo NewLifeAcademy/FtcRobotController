3c62f9bd5475b5f533e595c9500d6101d6796d8f
Benjamin Spain <2678462+bspain@users.noreply.github.com>
2025-09-13 15:01:44 -0500

    drive via exported companion functions

--- TeamCodeSpain/src/main/java/org/firstinspires/ftc/teamcodespain/SpainBotBasicDrive.java
+++ TeamCodeSpain/src/main/java/org/firstinspires/ftc/teamcodespain/SpainBotBasicDrive.java
@@ -32,11 +32,16 @@ package org.firstinspires.ftc.teamcodespain;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Drawing;
 import org.firstinspires.ftc.teamcodespain.SpainBot2025;
 
 /*
@@ -54,7 +59,29 @@ public class SpainBotBasicDrive extends LinearOpMode {
 
         waitForStart();
 
-        if (opModeIsActive()) {
+        // implementation from LocalizationTest.java
+        while (opModeIsActive()) {
+            robot.setDrivePowers(new PoseVelocity2d(
+                    new Vector2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x
+                    ),
+                    -gamepad1.right_stick_x
+            ));
+
+            robot.updatePoseEstimate();
+
+            Pose2d pose = robot.localizer.getPose();
+            telemetry.addData("x", pose.position.x);
+            telemetry.addData("y", pose.position.y);
+            telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
+            telemetry.update();
+
+            TelemetryPacket packet = new TelemetryPacket();
+            packet.fieldOverlay().setStroke("#3F51B5");
+            Drawing.drawRobot(packet.fieldOverlay(), pose);
+            FtcDashboard.getInstance().sendTelemetryPacket(packet);
+
         }
     }   // end runOpMode()
 }   // end class
