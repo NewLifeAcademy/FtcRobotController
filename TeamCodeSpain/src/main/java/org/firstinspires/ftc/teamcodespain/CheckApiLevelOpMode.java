2786f06b9e5b579c75d182979d3e8dd172615b4c
Benjamin Spain <2678462+bspain@users.noreply.github.com>
2025-11-15 13:36:23 -0600

    remove jackson databind: fix roadrunning tuning crash

---
+++ TeamCodeSpain/src/main/java/org/firstinspires/ftc/teamcodespain/CheckApiLevelOpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcodespain;
+
+import android.os.Build;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Check Android API Level")
+public class CheckApiLevelOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // Wait for start
+        waitForStart();
+        if (opModeIsActive()) {
+            int apiLevel = Build.VERSION.SDK_INT;
+            telemetry.addData("Android API Level", apiLevel);
+            telemetry.addData("Android Version", Build.VERSION.RELEASE);
+            telemetry.update();
+            while (opModeIsActive()) {
+                idle();
+            }
+        }
+    }
+}
