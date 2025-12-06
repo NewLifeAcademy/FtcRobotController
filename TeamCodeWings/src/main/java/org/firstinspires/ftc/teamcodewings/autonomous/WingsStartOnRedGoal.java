ebd3c6602d5634228b4885ba5aaabbeb44d31f5f
New Life Academy Robotics <150531206+nlarobotics@users.noreply.github.com>
2025-12-06 17:00:09 -0600

    flywheel adjust

--- TeamCodeWings/src/main/java/org/firstinspires/ftc/teamcodewings/autonomous/WingsStartOnRedGoal.java
+++ TeamCodeWings/src/main/java/org/firstinspires/ftc/teamcodewings/autonomous/WingsStartOnRedGoal.java
@@ -20,7 +20,7 @@ public class WingsStartOnRedGoal extends LinearOpMode {
 
     public static int FIRE_TIME = 5;
     public static int FLYWHEEL_SPINUP_TIME = 2;
-    public static double FLYWHEEL_POWER = 0.9;
+    public static double FLYWHEEL_POWER = 0.85;
     public static double START_POSE_X = -48;
     public static double START_POSE_Y = 48;
     public static double START_HEADING = 315;
